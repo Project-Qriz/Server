c482fff8b4c313bd07322c0e6e75a54cf7601e69
백승주 <137677440+bearkuang@users.noreply.github.com>
2025-05-18 15:11:41 +0900

    fix : improve refresh token renewal logic

--- src/main/java/com/qriz/sqld/config/jwt/JwtVO.java
+++ src/main/java/com/qriz/sqld/config/jwt/JwtVO.java
@@ -2,8 +2,8 @@ package com.qriz.sqld.config.jwt;
 
 public interface JwtVO {
     public static final String SECRET = "qriz"; // HS256 (대칭키)
-    public static final int ACCESS_TOKEN_EXPIRATION_TIME = 60 * 60 * 6; // 6 시간
-    public static final int REFRESH_TOKEN_EXPIRATION_TIME = 60 * 60 * 24 * 30; // 7일
+    public static final long ACCESS_TOKEN_EXPIRATION_TIME = 60L * 60 * 6 * 1000; // 6 시간
+    public static final long REFRESH_TOKEN_EXPIRATION_TIME = 60L * 60 * 24 * 60 * 1000; // 60일
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER = "Authorization";
     public static final String REFRESH_HEADER = "Refresh-Token";
